4098e3b9f2e1b2e8d750f360b133e0fe96e6bdd3
Brodie Berger <brodieberger@gmail.com>
2026-08-24 19:00:00 -0400

    cobblestone test

--- common/src/main/java/me/stingrays110/chippedancient/ExampleMod.java
+++ common/src/main/java/me/stingrays110/chippedancient/ExampleMod.java
@@ -1,9 +1,13 @@
 package me.stingrays110.chippedancient;
 
+import me.stingrays110.chippedancient.registry.ModBlocks;
+import me.stingrays110.chippedancient.registry.ModItems;
+
 public final class ExampleMod {
     public static final String MOD_ID = "chipped_ancient";
 
     public static void init() {
-        // Write common init code here.
+        ModBlocks.BLOCKS.init();
+        ModItems.ITEMS.init();
     }
 }
