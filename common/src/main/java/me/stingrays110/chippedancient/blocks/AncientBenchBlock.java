e1098b2faf22b579dbb445aba16f051c6ce5caed
Brodie Berger <40439538+brodieberger@users.noreply.github.com>
2026-08-24 22:58:52 -0400

    acts as regular chipped bench

--- common/src/main/java/me/stingrays110/chippedancient/blocks/AncientBenchBlock.java
+++ common/src/main/java/me/stingrays110/chippedancient/blocks/AncientBenchBlock.java
@@ -1,12 +1,18 @@
 package me.stingrays110.chippedancient.blocks;
 
 import com.mojang.serialization.MapCodec;
+import earth.terrarium.chipped.common.menus.WorkbenchMenuProvider;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.BlockHitResult;
 
 public class AncientBenchBlock extends HorizontalDirectionalBlock {
     public static final MapCodec<AncientBenchBlock> CODEC = simpleCodec(AncientBenchBlock::new);
@@ -25,6 +31,12 @@ public class AncientBenchBlock extends HorizontalDirectionalBlock {
     @Override
     public BlockState getStateForPlacement(BlockPlaceContext context) {
         return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    @Override
+    protected InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hitResult) {
+        player.openMenu(new WorkbenchMenuProvider(getName()));
+        return InteractionResult.sidedSuccess(level.isClientSide);
     }
 
     @Override
